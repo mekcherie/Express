b932cf1b19eee1cea8adacd8e48f1f5876034dca
mekcherie <69826367+mekcherie@users.noreply.github.com>
2020-10-05 23:22:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var oneElement =document.querySelector("#width")
+var oneElement =document.querySelector("#height")
+var oneElement =document.querySelector("#color")
+
+oneElement.addEventListener("click", dosomething,)
