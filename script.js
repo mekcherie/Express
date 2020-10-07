693356470666d590aad16a828a35ada4070bf67e
mekcherie <69826367+mekcherie@users.noreply.github.com>
2020-10-06 17:13:28 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,29 +1,53 @@
-const display_info_container = document.getElementById('display-info');
-const draw_info_container = document.getElementById('draw-info');
-const user_input = document.getElementById('user-input');
-function display_info() {
-	let infoStr = '';
-	let width_input = document.getElementById('box-width');
-	let height_input = document.getElementById('box-height');
-	let color_input = document.getElementById('box-color');
-	infoStr += `<h2>Width</h2> <p>${width_input.value} px</p>
-    <h2>Height</h2> <p>${height_input.value} px</p>
-    <h2>Color</h2> <p>${color_input.value} px</p>`;
-	display_info_container.innerHTML = infoStr;
+// headline
+const headline_display_container = document.getElementById('headline-display');
+function add_headline() {
+    let headlineStr = '' ;
+    const headline_input = document.getElementById('enter-text');
+    headlineStr += '<h1 id= "headline">${headline_input.vallue}</h1>';
 }
-display_info();
-
-function draw_info() {
-	let infoStr = '';
-	let width_input = document.getElementById('box-width');
-	let height_input = document.getElementById('box-height');
-	let color_input = document.getElementById('box-color');
-	infoStr += `<h1>Color</h1> <div style="width:${width_input.value}px; height:${height_input.value}px; background-color:${color_input.value}"></div>`;
-	draw_info_container.innerHTML = infoStr;
+add_headline();
+// the form input
+const display_target = document.getElementById('headline');
+function update_font_size() {
+    const sizeInput = document.getElementById('input-size');
+    display_target.style.fontSize = parseInt(sizeInput.value) + 'px';
 }
-draw_info();
+function update_font_family() {
+    const fontfamily_input = document.getElementById('select-font');
+    display_target.style.fontfamily = fontfamily_input.value;
+}
+function update_bg_color() {
+    const bg_color_input = document.getElementById('input-bg-color');
+    display_target.style.backgroundColor = bg_color_input.value;
+}
+function update_color() {
+    const color_input = document.getElementById('input-color');
+    display_target.style.Color = color_input.value;
+}
+function update_headline() {
+    const headline_input = document.getElementById('enter-text');
+    display_target.innerHTML = headline_input.value;
+}
+// updating
+update_font_size();
+update_font_family();
+update_bg_color();
+update_color();
+update_headline();
 
-user_input.onchange = function (e) {
-	display_info();
-	draw_info();
-};
+// handling
+const data_input = document.getElementById('data-input');
+data_input.onchange = function (e) {
+    show_data_output();
+    if (e.target && e.target.id === 'input-size') {
+        update_font_size();
+    } else if (e.target && e.target.id === 'select-font') {
+        update_fontfamily();
+    } else if (e.target && e.target.id === 'input-color') {
+        update_color();
+    } else if (e.target && e.target.id === 'input-bg-color') {
+        update_bg_color();
+    }
+}
+const headline_input = document.getElementById('enter-text');
+headline_input.addEventListener('input', update_headline);
