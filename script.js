101b45b0eff10c82c22dbf6ac321180b28814f86
mekcherie <69826367+mekcherie@users.noreply.github.com>
2020-10-07 17:38:37 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,53 +1,54 @@
-// headline
-const headline_display_container = document.getElementById('headline-display');
-function add_headline() {
-    let headlineStr = '' ;
-    const headline_input = document.getElementById('enter-text');
-    headlineStr += '<h1 id= "headline">${headline_input.vallue}</h1>';
-}
-add_headline();
-// the form input
-const display_target = document.getElementById('headline');
-function update_font_size() {
-    const sizeInput = document.getElementById('input-size');
-    display_target.style.fontSize = parseInt(sizeInput.value) + 'px';
-}
-function update_font_family() {
-    const fontfamily_input = document.getElementById('select-font');
-    display_target.style.fontfamily = fontfamily_input.value;
-}
-function update_bg_color() {
-    const bg_color_input = document.getElementById('input-bg-color');
-    display_target.style.backgroundColor = bg_color_input.value;
-}
-function update_color() {
-    const color_input = document.getElementById('input-color');
-    display_target.style.Color = color_input.value;
-}
-function update_headline() {
-    const headline_input = document.getElementById('enter-text');
-    display_target.innerHTML = headline_input.value;
-}
-// updating
-update_font_size();
-update_font_family();
-update_bg_color();
-update_color();
-update_headline();
+// The headline element
+const display = document.querySelector('#display')
+​
+// The input for size
+const inputSize = document.querySelector('#input-size')
+// The element that displays the value 
+const showSize = document.querySelector('#show-size')
+// Repeat this for the input and "show" elements
+​
+// adding event listener cause thats how it functions 
+inputSize.addEventListener('input', handleInput)
 
-// handling
-const data_input = document.getElementById('data-input');
-data_input.onchange = function (e) {
-    show_data_output();
-    if (e.target && e.target.id === 'input-size') {
-        update_font_size();
-    } else if (e.target && e.target.id === 'select-font') {
-        update_fontfamily();
-    } else if (e.target && e.target.id === 'input-color') {
-        update_color();
-    } else if (e.target && e.target.id === 'input-bg-color') {
-        update_bg_color();
-    }
+const inputFont = document.querySelector('#select-font')
+const showFont = document.querySelector('#show-font')
+inputFont.addEventListener('input', handleInput)
+​
+const inputColor = document.querySelector('#input-color')
+const showColor = document.querySelector('#show-color')
+inputColor.addEventListener('input', handleInput)
+​
+const selectedBG = document.querySelector('#input-bg-color')
+const showBGvalue = document.querySelector('#show-bg-color')
+selectedBG.addEventListener('input', handleInput)
+​
+const inputText = document.querySelector('#enter-text')
+const showText = document.querySelector('#display')
+inputText.addEventListener('input', handleInput)
+​
+// style properties 
+function handleInput() {
+	// font size:
+	const fontSize = inputSize.value
+	// style 
+	display.style.fontSize = fontSize
+	// size
+    showSize.innerHTML = fontSize
+    
+    const selectedFont = inputFont.value
+    display.style.selectedFont = selectedFont
+    showFont.innerHTML = selectedFont
+​
+    const fontColor = inputColor.value
+    display.style.fontColor = fontColor
+    showColor.innerHTML = fontColor
+​
+    const bgColor = selectedBG.value
+    display.style.bgColor = bgColor
+    showBGvalue.innerHTML = bgColor
+​
+    const userText = inputText.value
+    display.style.inputText = inputText
+    showText.innerHTML = userText
 }
-const headline_input = document.getElementById('enter-text');
-headline_input.addEventListener('input', update_headline);
+headline(); 
